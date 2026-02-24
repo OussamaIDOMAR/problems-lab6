c0fef26706e3ffd80a6d4e7e01a5a7ec7891881c
Oussama <o.idomar5161@uca.ac.ma>
2026-02-24 18:59:00 +0000

    Implement BookQueue class with basic operations

--- src/problem5/BookQueue.java
+++ src/problem5/BookQueue.java
@@ -1,4 +1,24 @@
 package problem5;
 
+import java.util.LinkedList;
+
 public class BookQueue {
+    private LinkedList<Book> list = new LinkedList<>();
+
+    public void enqueue(Book b) {
+        list.addLast(b);
+    }
+
+    public Book dequeue() {
+        return list.removeFirst();
+    }
+
+    public Book peek() {
+        return list.getFirst();
+    }
+
+    public void printQueue() {
+        for (Book b : list)
+            System.out.println("- " + b);
+    }
 }
